031b772e876660bfe6ab175e7f5af4534caf54e3
georgeweigt <gweigt@icloud.com>
2020-10-24 14:17:51 -0700

    Update eigenmath.js

--- eigenmath.js
+++ eigenmath.js
@@ -2,12 +2,11 @@
 
 <html>
 <body>
-<script src="https://eigenmath.github.io/eigenmath.js"></script>
+<script src="https://georgeweigt.github.io/eigenmath.js"></script>
 <textarea id="stdin" rows="24" cols="80" style="font-family:courier;font-size:12pt">
 -- Eigenmath script goes here, for example:
 draw(sin(x))
 </textarea>
-<p>
 <button onclick="run()" style="font-size:20pt">Run</button>
 <p>
 <div id="stdout"></div>
