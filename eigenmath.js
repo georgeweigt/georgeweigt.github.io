26e1d302959d22800362c1a742f93a39e9f7e55a
georgeweigt <gweigt@icloud.com>
2020-10-28 14:59:36 -0700

    Update eigenmath.js

--- eigenmath.js
+++ eigenmath.js
@@ -4246,12 +4246,11 @@ emit_symbol_scan(s, k)
 function
 emit_symbol_text(u, s, small_font)
 {
-	if (s.length == 1) {
-		if (isdigit(s))
-			emit_roman_text(u, s, small_font);
-		else
-			emit_italic_text(u, s, small_font);
-	} else
+	if (isdigit(s))
+		emit_roman_text(u, s, small_font);
+	else if (s.length == 1)
+		emit_italic_text(u, s, small_font);
+	else
 		emit_glyph(u, s, small_font);
 }
 function
