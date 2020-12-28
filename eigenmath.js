6db6c13fc921b9e233d67587f355c8a390c6ce47
georgeweigt <gweigt@icloud.com>
2020-12-28 12:56:07 -0700

    Update eigenmath.js

--- eigenmath.js
+++ eigenmath.js
@@ -5703,9 +5703,6 @@ eval_run(p1)
 
 	inbuf = f.responseText;
 
-//print_buf(inbuf, BLUE);
-
-
 	k = 0;
 
 	for (;;) {
@@ -11421,31 +11418,14 @@ run()
 function
 run_nib()
 {
-	var k, t;
+	var k = 0;
 
+	inbuf = document.getElementById("stdin").value;
 	stdout = document.getElementById("stdout");
-
 	stdout.innerHTML = "";
 
 	init();
 	initscript();
-
-	t = document.getElementById("stdlib");
-
-	if (t != null) {
-		inbuf = t.contentDocument.body.childNodes[0].textContent;
-		k = 0;
-		for (;;) {
-			k = scan_inbuf(k);
-			if (k == 0)
-				break; // end of input
-			eval_and_print_result();
-		}
-	}
-
-	inbuf = document.getElementById("stdin").value;
-
-	k = 0;
 
 	for (;;) {
 
