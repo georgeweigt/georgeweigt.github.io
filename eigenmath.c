879fb9bc2f76aa850a131a4beed467291ac843ed
georgeweigt <gweigt@icloud.com>
2021-03-14 12:18:47 -0700

    update

--- eigenmath.c
+++ eigenmath.c
@@ -1,4 +1,4 @@
-/* March 13, 2021
+/* March 14, 2021
 
 To build and run:
 
@@ -362,6 +362,7 @@ extern int primetab[MAXPRIMETAB];
 void eval_abs(void);
 void absv(void);
 void absv_nib(void);
+void absv_tensor(void);
 void eval_add(void);
 void add(void);
 void add_terms(int n);
@@ -1149,22 +1150,68 @@ absv(void)
 void
 absv_nib(void)
 {
+	int h;
 	p1 = pop();
 	if (istensor(p1)) {
-		if (p1->u.tensor->ndim != 1)
-			stop("abs");
+		absv_tensor();
+		return;
+	}
+	if (isnum(p1)) {
+		push(p1);
+		if (isnegativenumber(p1))
+			negate();
+		return;
+	}
+	if (iscomplexnumber(p1)) {
 		push(p1);
 		push(p1);
 		conjugate();
-		inner();
-		sqrtv();
+		multiply();
+		push_rational(1, 2);
+		power();
 		return;
 	}
+	// abs(1/a) evaluates to 1/abs(a)
+	if (car(p1) == symbol(POWER) && isnegativeterm(caddr(p1))) {
+		push(p1);
+		reciprocate();
+		absv();
+		reciprocate();
+		return;
+	}
+	// abs(a*b) evaluates to abs(a)*abs(b)
+	if (car(p1) == symbol(MULTIPLY)) {
+		h = tos;
+		p1 = cdr(p1);
+		while (iscons(p1)) {
+			push(car(p1));
+			absv();
+			p1 = cdr(p1);
+		}
+		multiply_factors(tos - h);
+		return;
+	}
+	if (isnegativeterm(p1) || (car(p1) == symbol(ADD) && isnegativeterm(cadr(p1)))) {
+		push(p1);
+		negate();
+		p1 = pop();
+	}
+	push_symbol(ABS);
+	push(p1);
+	list(2);
+}
+
+void
+absv_tensor(void)
+{
+	if (p1->u.tensor->ndim != 1)
+		stop("abs");
 	push(p1);
 	push(p1);
 	conjugate();
-	multiply();
-	sqrtv();
+	inner();
+	push_rational(1, 2);
+	power();
 }
 
 void
