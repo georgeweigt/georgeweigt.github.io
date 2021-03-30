cad567ba586e08e5a57875e6268175c279c4f08d
georgeweigt <gweigt@icloud.com>
2021-03-30 11:41:43 -0700

    Update eigenmath.js

--- eigenmath.js
+++ eigenmath.js
@@ -8073,7 +8073,7 @@ inner()
 function
 inrange(x, y)
 {
-	return x >= 0 && x <= DRAW_WIDTH && y >= 0 && y <= DRAW_HEIGHT;
+	return x > -0.5 && x < DRAW_WIDTH + 0.5 && y > -0.5 && y < DRAW_HEIGHT + 0.5;
 }
 function
 integral()
