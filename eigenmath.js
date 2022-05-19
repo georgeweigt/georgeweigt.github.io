c6401b83d39f362132d53c4a923cb0c2b99db082
georgeweigt <gweigt@icloud.com>
2022-05-19 08:12:51 -0700

    Update eigenmath.js

--- eigenmath.js
+++ eigenmath.js
@@ -15625,6 +15625,8 @@ simplify()
 		return;
 	}
 
+	// already simple?
+
 	if (!iscons(p1)) {
 		push(p1);
 		return;
@@ -15642,14 +15644,10 @@ simplify()
 
 	list(stack.length - h);
 	evalf();
-	p1 = pop();
-	push(p1);
-
-	if (!iscons(p1))
-		return;
 
 	simplify_pass1();
-	simplify_pass2();
+	simplify_pass2(); // try exponential form
+	simplify_pass3(); // try rectangular form
 }
 function
 simplify_pass1()
@@ -15657,6 +15655,13 @@ simplify_pass1()
 	var p1, NUM, DEN, R, T;
 
 	p1 = pop();
+
+	// already simple?
+
+	if (!iscons(p1)) {
+		push(p1);
+		return;
+	}
 
 	if (car(p1) == symbol(ADD)) {
 		push(p1);
@@ -15733,6 +15738,8 @@ simplify_pass1()
 
 	push(p1);
 }
+// try exponential form
+
 function
 simplify_pass2()
 {
@@ -15740,14 +15747,55 @@ simplify_pass2()
 
 	p1 = pop();
 
+	// already simple?
+
+	if (!iscons(p1)) {
+		push(p1);
+		return;
+	}
+
 	push(p1);
 	circexp();
 	rationalize();
 	evalf(); // to normalize
 	p2 = pop();
 
-	if (complexity(p2) < complexity(p1))
-		p1 = p2;
+	if (complexity(p2) < complexity(p1)) {
+		push(p2);
+		return;
+	}
+
+	push(p1);
+}
+// try rectangular form
+
+function
+simplify_pass3()
+{
+	var p1, p2;
+
+	p1 = pop();
+
+	// already simple?
+
+	if (!iscons(p1)) {
+		push(p1);
+		return;
+	}
+
+	if (car(p1) != symbol(ADD) || !findf(p1, imaginaryunit)) {
+		push(p1);
+		return;
+	}
+
+	push(p1);
+	rect();
+	p2 = pop();
+
+	if (!iscons(p2)) {
+		push(p2);
+		return;
+	}
 
 	push(p1);
 }
