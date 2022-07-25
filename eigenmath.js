df48f1e41d5936b4ae7edb395b2386b3bd1d8bc9
georgeweigt <gweigt@icloud.com>
2022-07-25 16:51:02 -0700

    Update eigenmath.js

--- eigenmath.js
+++ eigenmath.js
@@ -2425,6 +2425,7 @@ const MOD = "mod";
 const NIL = "nil";
 const NOEXPAND = "noexpand";
 const NOT = "not";
+const NROOTS = "nroots";
 const NUMBER = "number";
 const NUMERATOR = "numerator";
 const OR = "or";
@@ -12592,6 +12593,303 @@ normalize_power_factors(h)
 		}
 	}
 }
+const DELTA = 1e-6;
+const EPSILON = 1e-9;
+
+function
+eval_nroots(p1)
+{
+	push(cadr(p1));
+	evalf();
+
+	p1 = cddr(p1);
+
+	if (iscons(p1)) {
+		push(car(p1));
+		evalf();
+	} else
+		push_symbol(SYMBOL_X);
+
+	nroots();
+}
+
+function
+nroots()
+{
+	var h, i, n;
+	var A, P, X, RE, IM;
+	var ar, ai, mag;
+	var cr, ci;
+	var tr, ti;
+
+	cr = [];
+	ci = [];
+
+	tr = [];
+	ti = [];
+
+	X = pop();
+	P = pop();
+
+	h = stack.length;
+
+	coeffs(P, X); // put coeffs on stack
+
+	n = stack.length - h;
+
+	if (n == 1) {
+		stack[h] = symbol(NIL); // P is just a constant, no roots
+		return;
+	}
+
+	// convert coeffs to doubles
+
+	for (i = 0; i < n; i++) {
+
+		push(stack[h + i]);
+		real();
+		floatfunc();
+		RE = pop();
+
+		push(stack[h + i]);
+		imag();
+		floatfunc();
+		IM = pop();
+
+		if (!isdouble(RE) || !isdouble(IM))
+			stopf("nroots: coeffs");
+
+		cr[i] = RE.d;
+		ci[i] = IM.d;
+	}
+
+	stack.splice(h); // pop all
+
+	while (n > 1) {
+
+		nfindroot(cr, ci, n, tr, ti);
+
+		ar = tr[0];
+		ai = ti[0];
+
+		mag = zabs(ar, ai);
+
+		if (Math.abs(ar / mag) < DELTA)
+			ar = 0.0;
+
+		if (Math.abs(ai / mag) < DELTA)
+			ai = 0.0;
+
+		// push root
+
+		push_double(ar);
+		push_double(ai);
+		push(imaginaryunit);
+		multiply();
+		add();
+
+		// divide by X - A
+
+		nreduce(cr, ci, n, ar, ai);
+
+		n--;
+	}
+
+	n = stack.length - h;
+
+	if (n == 1)
+		return; // only 1 root
+
+	sort(n);
+
+	A = alloc_vector(n);
+
+	for (i = 0; i < n; i++)
+		A.elem[i] = stack[h + i];
+
+	stack.splice(h); // pop all
+
+	push(A);
+}
+
+// uses secant method
+
+function
+nfindroot(cr, ci, n, ar, ai)
+{
+	var i, j;
+	var t;
+	var br, dfr, dxr, far, fbr, tr, xr, yr;
+	var bi, dfi, dxi, fai, fbi, ti, xi, yi;
+
+	tr = [];
+	ti = [];
+
+	// divide by leading coeff
+
+	br = cr[n - 1];
+	bi = ci[n - 1];
+
+	t = br * br + bi * bi;
+
+	for (i = 0; i < n - 1; i++) {
+		cr[i] = (cr[i] * br + ci[i] * bi) / t;
+		ci[i] = (ci[i] * br - cr[i] * bi) / t;
+	}
+
+	cr[n - 1] = 1.0;
+	ci[n - 1] = 0.0;
+
+	// if const term is small then root is 0
+
+	if (zabs(cr[0], ci[0]) < EPSILON) {
+		ar[0] = 0.0;
+		ai[0] = 0.0;
+		return;
+	}
+
+	for (i = 0; i < 100; i++) {
+
+		ar[0] = urandom();
+		ai[0] = urandom();
+
+		fata(cr, ci, n, ar[0], ai[0], tr, ti);
+
+		far = tr[0];
+		fai = ti[0];
+
+		br = ar[0];
+		bi = ai[0];
+
+		fbr = far;
+		fbi = fai;
+
+		ar[0] = urandom();
+		ai[0] = urandom();
+
+		for (j = 0; j < 1000; j++) {
+
+			fata(cr, ci, n, ar[0], ai[0], tr, ti);
+
+			far = tr[0];
+			fai = ti[0];
+
+			if (zabs(far, fai) < EPSILON)
+				return;
+
+			if (zabs(far, fai) < zabs(fbr, fbi)) {
+
+				xr = ar[0];
+				xi = ai[0];
+
+				ar[0] = br;
+				ai[0] = bi;
+
+				br = xr;
+				bi = xi;
+
+				xr = far;
+				xi = fai;
+
+				far = fbr;
+				fai = fbi;
+
+				fbr = xr;
+				fbi = xi;
+			}
+
+			// dx = b - a
+
+			dxr = br - ar[0];
+			dxi = bi - ai[0];
+
+			// df = fb - fa
+
+			dfr = fbr - far;
+			dfi = fbi - fai;
+
+			// y = dx / df
+
+			t = dfr * dfr + dfi * dfi;
+
+			if (t == 0.0)
+				break;
+
+			yr = (dxr * dfr + dxi * dfi) / t;
+			yi = (dxi * dfr - dxr * dfi) / t;
+
+			// a = b - y * fb
+
+			ar[0] = br - (yr * fbr - yi * fbi);
+			ai[0] = bi - (yr * fbi + yi * fbr);
+		}
+	}
+
+	stopf("nroots: convergence error");
+}
+
+// compute f at a
+
+function
+fata(cr, ci, n, ar, ai, far, fai)
+{
+	var k;
+	var t, xr, xi;
+
+	// x = a
+
+	xr = ar;
+	xi = ai;
+
+	// fa = c0 + c1 * x
+
+	far[0] = cr[0] + cr[1] * xr - ci[1] * xi;
+	fai[0] = ci[0] + cr[1] * xi + ci[1] * xr;
+
+	for (k = 2; k < n; k++) {
+
+		// x = a * x
+
+		t = ar * xr - ai * xi;
+		xi = ar * xi + ai * xr;
+		xr = t;
+
+		// fa += c[k] * x
+
+		far[0] += cr[k] * xr - ci[k] * xi;
+		fai[0] += cr[k] * xi + ci[k] * xr;
+	}
+}
+
+// divide by x - a
+
+function
+nreduce(cr, ci, n, ar, ai)
+{
+	var k;
+
+	for (k = n - 1; k > 0; k--) {
+		cr[k - 1] += cr[k] * ar - ci[k] * ai;
+		ci[k - 1] += ci[k] * ar + cr[k] * ai;
+	}
+
+	for (k = 0; k < n - 1; k++) {
+		cr[k] = cr[k + 1];
+		ci[k] = ci[k + 1];
+	}
+}
+
+function
+zabs(r, i)
+{
+	return Math.sqrt(r * r + i * i);
+}
+
+function
+urandom()
+{
+	return 4.0 * Math.random() - 2.0;
+}
 function
 numerator()
 {
@@ -16250,6 +16548,7 @@ var symtab = {
 "nil":		{printname:NIL,		func:eval_nil},
 "noexpand":	{printname:NOEXPAND,	func:eval_noexpand},
 "not":		{printname:NOT,		func:eval_not},
+"nroots":	{printname:NROOTS,	func:eval_nroots},
 "number":	{printname:NUMBER,	func:eval_number},
 "numerator":	{printname:NUMERATOR,	func:eval_numerator},
 "or":		{printname:OR,		func:eval_or},
