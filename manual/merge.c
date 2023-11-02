7cd56b7ca64eedc1c8ece1cafd7a15685db14e86
georgeweigt <55809489+georgeweigt@users.noreply.github.com>
2023-11-02 06:05:30 -0700

    update

---
+++ manual/merge.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void emit(char *);
+
+char buf[1000];
+
+int
+main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 1; i < argc; i++)
+		emit(argv[i]);
+
+	fputs("\\end{document}\n", stdout);
+}
+
+void
+emit(char *filename)
+{
+	FILE *f;
+
+//	printf("\\bigskip\n");
+
+	f = fopen(filename, "r");
+
+	if (f == NULL) {
+		fprintf(stderr, "cannot open %s\n", filename);
+		exit(1);
+	}
+
+	while (fgets(buf, sizeof buf, f))
+		if (strcmp(buf, "\\begin{document}\n") == 0)
+			break;
+
+	while (fgets(buf, sizeof buf, f)) {
+		if (strncmp(buf, "\\section*", 9) == 0) {
+			fputs("\\section", stdout);
+			fputs(buf + 9, stdout);
+			continue;
+		}
+		if (strcmp(buf, "\\end{document}\n") == 0)
+			break;
+		fputs(buf, stdout);
+	}
+
+	fclose(f);
+}
