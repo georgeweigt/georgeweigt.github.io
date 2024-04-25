30f48f1d99b446bea35bf1b29ffe21515664590b
georgeweigt <55809489+georgeweigt@users.noreply.github.com>
2024-04-25 15:26:50 -0700

    Update eigenmath.js

--- eigenmath.js
+++ eigenmath.js
@@ -17053,7 +17053,7 @@ static_reciprocate()
 		return;
 	}
 
-	if (car(p2) == symbol(POWER)) {
+	if (car(p2) == symbol(POWER) && isnum(caddr(p2))) {
 		push_symbol(POWER);
 		push(cadr(p2));
 		push(caddr(p2));
