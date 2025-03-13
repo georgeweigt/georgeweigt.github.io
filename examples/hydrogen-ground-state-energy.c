f75fb00868409bc6b72fc98ebf861fc60485c442
georgeweigt <55809489+georgeweigt@users.noreply.github.com>
2025-03-13 11:27:49 -0700

    update

--- examples/hydrogen-ground-state-energy.c
+++ examples/hydrogen-ground-state-energy.c
@@ -7,12 +7,12 @@ main()
 	double c = 299792458.0;
 	double e = 1.602176634e-19;
 	double Ry = 13.605693122990;
-	double me = 9.1093837015e-31;
-	double mp = 1.67262192369e-27;
+	double me = 9.1093837139e-31;
+	double mp = 1.67262192595e-27;
 
 	double mu = me * mp / (me + mp);
 
 	double E = mu * c * c * alpha * alpha / 2.0 / e;
 
-	printf("%20.8f\n", E);
+	printf("%20.10f\n", E);
 }
